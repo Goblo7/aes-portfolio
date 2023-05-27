0ec2686e59cc3ac70e46c6c0c9c399f8b86266d4
Ahmed Ehab <ahmed.ehab632@gmail.com>
2023-05-27 16:38:47 +0300

    update v0.31

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import { useState, useEffect } from "react";
-import { layoutStyles } from "./styles/layout.styles";
-import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
 import Layout from "./layout/layout.components";
 import Preloader from "./components/preloader/preloader.component";
 import Header from "./components/header/header.component";
 import Sections from "./components/sections//section-map.component";
 import Footer from "./components/footer/footer.component";
-
-const LayoutTheme = createTheme({
-  palette: {
-    mode: "dark",
-    background: {
-      default: layoutStyles.mainColor,
-      paper: layoutStyles.paperColor,
-    },
-  },
-  typography: {
-    fontFamily: layoutStyles.mainFontFamily,
-    button: {
-      fontFamily: layoutStyles.secandryFontFamily,
-      textTransform: "none",
-    },
-  },
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
-      overSize: 1700,
-    },
-  },
-});
 
 export default function App() {
   const [preloader, setPreLoader] = useState(false);
@@ -44,8 +15,7 @@ export default function App() {
   }, []);
 
   return (
-    <ThemeProvider theme={LayoutTheme}>
-      <CssBaseline />
+    <>
       {preloader ? (
         <Preloader />
       ) : (
@@ -55,6 +25,6 @@ export default function App() {
           <Footer />
         </Layout>
       )}
-    </ThemeProvider>
+    </>
   );
 }
--- src/layout/layout.components.jsx
+++ src/layout/layout.components.jsx
@@ -1,7 +1,41 @@
 import { Box } from "@mui/material";
+import { layoutStyles } from "../styles/layout.styles";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+
+const LayoutTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: layoutStyles.mainColor,
+      paper: layoutStyles.paperColor,
+    },
+  },
+  typography: {
+    fontFamily: layoutStyles.mainFontFamily,
+    button: {
+      fontFamily: layoutStyles.secandryFontFamily,
+      textTransform: "none",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+      overSize: 1700,
+    },
+  },
+});
 
 export const Layout = ({ children: Children }) => {
-  return <Box>{Children}</Box>;
+  return (
+    <ThemeProvider theme={LayoutTheme}>
+      <CssBaseline />
+      <Box>{Children}</Box>
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
