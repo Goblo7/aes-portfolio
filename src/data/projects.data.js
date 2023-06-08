59aec2ee894424803739224b8fb3479a298159d8
Ahmed Ehab <ahmed.ehab632@gmail.com>
2023-06-08 23:11:35 +0300

    update v-0.63

--- src/data/projects.data.js
+++ src/data/projects.data.js
@@ -8,22 +8,6 @@ const projectsData = [
       codeLink: "https://github.com/Goblo7/Krusty-restaurant",
       liveLink: "https://goblo7.github.io/Krusty-restaurant/",
     },
-    {
-      title: "Restaurant Menu",
-      description:
-        " A front-end website made with html, css and vanilla javascript with swipers library.",
-      tools: ["HTML", "CSS", "JavaScript"],
-      codeLink: "https://github.com/Goblo7/Krusty-restaurant",
-      liveLink: "https://goblo7.github.io/Krusty-restaurant/",
-    },
-    {
-      title: "Restaurant Menu",
-      description:
-        " A front-end website made with html, css and vanilla javascript with swipers library.",
-      tools: ["HTML", "CSS", "JavaScript"],
-      codeLink: "https://github.com/Goblo7/Krusty-restaurant",
-      liveLink: "https://goblo7.github.io/Krusty-restaurant/",
-    },
   ],
   [
     {
