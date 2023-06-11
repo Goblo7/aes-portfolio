771ec4902854466fd2808397b210fa4eb5a763ae
Ahmed Ehab <ahmed.ehab632@gmail.com>
2023-06-11 18:48:52 +0300

    cleaning up wrapper

--- src/components/sections/section/sections-wrapper.component.jsx
+++ src/components/sections/section/sections-wrapper.component.jsx
@@ -1,8 +1,6 @@
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { useRef } from "react";
 import { useInView } from "framer-motion";
-import { Box } from "@mui/material";
-
 const SectionWrapper = (props) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
@@ -11,15 +9,12 @@ const SectionWrapper = (props) => {
     transform: isInView ? "none" : "translateY(75px)",
     opacity: isInView ? 1 : 0,
     transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1)0.3s",
-    minHeight:"inherit"
   };
 
   return (
-    <Box ref={ref} sx={inView}>
-      <Grid container sx={props.className}>
+      <Grid ref={ref} container sx={[props.className, inView]}>
         {props.children}
       </Grid>
-    </Box>
   );
 };
 
